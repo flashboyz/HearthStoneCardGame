bc45188a8e8d2a6af33d374e7a1f0429f4aa3935
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-09-28 12:17:05 +0300

    Initial Commit

---
+++ HearthStoneCardGame/HearthStoneCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  HearthStoneCardGameApp.swift
+//  HearthStoneCardGame
+//
+//  Created by Константин Прокофьев on 28.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct HearthStoneCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
